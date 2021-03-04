710abe4326faad3d409b657d5ecaef95b8d0e8c7
twn9320 <twn9320@gmail.com>
2021-03-04 15:58:28 +0800

    Update D21_HW.py

--- homework/D21_HW.py
+++ homework/D21_HW.py
@@ -9,12 +9,13 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 # 取得資料集
+plt.figure(0)
 df = sns.load_dataset('titanic')
 ax = sns.barplot(x="sex", y="survived",hue = "class", data=df)
 
-
+plt.figure(1)
 df2 = sns.load_dataset('tips')
-print(df2)
-#sns.boxplot(x=, y=, data=)
-#sns.stripplot(x=, y=, data=, jitter=)
-#plt.show()
+print(df2.info())
+sns.boxplot(x='day', y='tip', data=df2)
+sns.stripplot(x='day', y='tip', data=df, jitter=True)
+plt.show()
